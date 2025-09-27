79bac319a2dd427561539691c81225a23594a82b
Muhammad Ahmad <highonworking@gmail.com>
2025-09-27 19:13:57 +0500

    Update main.py
    Cypher is indeed done. Wow!!!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import string
 #user input
 #Shift
 #Alphabets
@@ -5,13 +6,40 @@
 #replace orig with new
 #print new string
 def ceaser_cypher(sentence, shift):
-    pass
+    new_sentence = ""
+    alphabets = string.ascii_letters
+    for word in sentence:
+        if word in alphabets:
+            index = alphabets.index(word)
+            new_sentence += alphabets[(index + shift) % len(alphabets)]
+        else:
+            new_sentence += word
+    return new_sentence
+
+def decypher(sentence, shift):
+    new_sentence = ""
+    alphabets = string.ascii_letters
+    for word in sentence:
+        if word in alphabets:
+            index = alphabets.index(word)
+            new_sentence += alphabets[(index - shift) % len(alphabets)]
+        else:
+            new_sentence += word
+    return new_sentence
 
 def main():
-    sent = input("Enter the sentence you want to encrypt: ")
-    shift = int(input("Enter the shift value: "))
-    encrypted_sentence = ceaser_cypher(sent, shift)
-    print("Encrypted sentence:", encrypted_sentence)
+    option = input("Type 'e' to encrypt or 'd' to decrypt: ").lower()
+    if option == 'd':
+        sent = input("Enter the sentence you want to decrypt: ")
+        shift = int(input("Enter the shift value: "))
+        decrypted_sentence = decypher(sent, shift)
+        print("Decrypted sentence:", decrypted_sentence)
+        return
+    elif option == 'e':
+        sent = input("Enter the sentence you want to encrypt: ")
+        shift = int(input("Enter the shift value: "))
+        encrypted_sentence = ceaser_cypher(sent, shift)
+        print("Encrypted sentence:", encrypted_sentence)
 
 if __name__ == "__main__":
     main()
